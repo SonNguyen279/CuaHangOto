685abe51b0b4a276e2f7bd945270ac0bc35e856c
SonNguyen279 <nguyenvansonln1102@gmail.com>
2024-09-22 15:35:30 +0700

    Gara

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header-main'>
       <div className='header-contents'>
       <h2>Đặt mẫu xe yêu thích của bạn tại đây</h2>
       <p>Người tiêu dùng không muốn một chiếc xe rẻ tiền, họ muốn có một chiếc xe hơi để phô trương. Một chiếc ô tô nói lên tình trạng và bản sắc cũng giống như phương tiện đi lại</p>
